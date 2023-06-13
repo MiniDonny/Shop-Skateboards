d6ad596021119a5a9e1da0d40d533fd06313824b
Donato Robledo <129457178+MiniDonny@users.noreply.github.com>
2023-06-13 17:48:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* const boton = documen.querySelector(".menu")
+function precionarInterruptor(){
+    const tema = document.body.classList.toggle('light')
+} */ //Si le hace click se apaga o se prende
+
+// Utilizar CSS para estilizar
+/*HMLT = creamos un button y le agregamos una clase con la etiqueta "oneclick" */ 
